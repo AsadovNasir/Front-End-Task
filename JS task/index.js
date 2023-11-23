65f70f8e5791ec1f4c954a4a4c2c7e2b5a980dfb
NasirAsadov <asadovnasir707@gmail.com>
2023-11-23 15:33:35 +0400

    JS Task Part 1

---
+++ JS task/index.js
@@ -0,0 +1,47 @@
+///////////////TASK 1///////////////////
+
+// "Spilt" vasitesi ile ayirdim
+// let fullName = 'Murad Nərimanlı Tərlan'
+// let fullNameArray = fullName.split('');
+//  console.log(fullNameArray)
+
+///////////////TASK 2///////////////////
+
+
+
+///////////////TASK 4///////////////////
+
+// let arr = [4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7];
+
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 5) {
+//         count++;
+//     }
+// }
+// console.log("5", count, "defe tekrarlanib");
+
+///////////////TASK 5///////////////////
+
+// let cem = arr.reduce((acc, current) => acc + current, 0);
+// console.log("cemi", cem);
+
+
+///////////////TASK 6///////////////////
+
+// let maxNum = arr[0]
+// for (let item of arr) {
+//     if (item > maxNum) {
+//         maxNum = item
+//     }
+// }
+
+// let count2 = maxNum;
+// for (let i = 0; i < arr.length; i++){
+//     if (arr [i] ===maxNum){
+//         count2++;
+//     }
+// }
+// console.log(maxNum, count2, "defe tekrarlanib");
+
+
